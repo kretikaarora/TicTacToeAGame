5f6108db049b21ce92159e430d77e153919e7508
kretika arora <kretikaarora@gmail.com>
2020-10-10 15:24:40 +0530

    [kretika arora] welcome to tictactoe

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TicTacToeGameSimulator
+{
+     public class TicTacToeGame
+    {
+        char[] board = new char[10];
+        bool[] positionOccupied = new bool[10];
+        static void Main(string[] args)
+        {
+            TicTacToeGame ticTacToeGame = new TicTacToeGame();
+            char[] board= ticTacToeGame.BoardFormation();
+        }
+        
+        public char[] BoardFormation()
+        {
+            // initialising new board 
+
+            int position;
+            // assigning empty value to board 
+            for (position = 1; position < board.Length; position++)
+            {
+                board[position] = ' ';
+                positionOccupied[position] = false;
+
+            }
+            return board;
+
+        }
+    }
+}
